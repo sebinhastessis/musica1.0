ae430800f2ec942a987b07b7569cfb2bb0056f40
sebinhastessis <134986110+sebinhastessis@users.noreply.github.com>
2023-05-29 20:26:40 -0600

    Add files via upload

---
+++ main.JS
@@ -0,0 +1,62 @@
+function function1() {
+    document.getElementById("audio1").play();
+    document.getElementById("audio2").pause();
+    document.getElementById("audio3").pause();
+    document.getElementById("audio4").pause();
+    document.getElementById("audio5").pause();
+  document.getElementById("im1").style.borderColor="green";
+  document.getElementById("im2").style.borderColor="aliceblue";
+  document.getElementById("im3").style.borderColor="aliceblue";
+  document.getElementById("im4").style.borderColor="aliceblue";
+  document.getElementById("im5").style.borderColor="aliceblue";
+  
+}
+		function function2() {
+            document.getElementById("audio1").pause();
+            document.getElementById("audio2").play();
+            document.getElementById("audio3").pause();
+            document.getElementById("audio4").pause();
+            document.getElementById("audio5").pause();
+       document.getElementById("im1").style.borderColor="aliceblue";
+  document.getElementById("im2").style.borderColor="green";
+  document.getElementById("im3").style.borderColor="aliceblue";
+  document.getElementById("im4").style.borderColor="aliceblue";
+  document.getElementById("im5").style.borderColor="aliceblue";
+}
+		function function3() {
+            document.getElementById("audio1").pause();
+            document.getElementById("audio2").pause();
+            document.getElementById("audio3").play();
+            document.getElementById("audio4").pause();
+            document.getElementById("audio5").pause();
+     document.getElementById("im1").style.borderColor="aliceblue";
+  document.getElementById("im2").style.borderColor="aliceblue";
+  document.getElementById("im3").style.borderColor="green";
+  document.getElementById("im4").style.borderColor="aliceblue";
+  document.getElementById("im5").style.borderColor="aliceblue";
+}
+		function function4() {
+            document.getElementById("audio1").pause();
+            document.getElementById("audio2").pause();
+            document.getElementById("audio3").pause();
+            document.getElementById("audio4").play();
+            document.getElementById("audio5").pause();
+     document.getElementById("im1").style.borderColor="aliceblue";
+  document.getElementById("im2").style.borderColor="aliceblue";
+  document.getElementById("im3").style.borderColor="aliceblue";
+  document.getElementById("im4").style.borderColor="green";
+  document.getElementById("im5").style.borderColor="aliceblue";
+}
+		function function5() {
+            document.getElementById("audio1").pause();
+            document.getElementById("audio2").pause();
+            document.getElementById("audio3").pause();
+            document.getElementById("audio4").pause();
+            document.getElementById("audio5").play();
+     document.getElementById("im1").style.borderColor="aliceblue";
+  document.getElementById("im2").style.borderColor="aliceblue";
+  document.getElementById("im3").style.borderColor="aliceblue";
+  document.getElementById("im4").style.borderColor="aliceblue";
+  document.getElementById("im5").style.borderColor="green";
+}
+
